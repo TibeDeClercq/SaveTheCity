f0e4cba6c0723425d64e3147f7a993098737ecbc
RubenVorsselmans <78792844+RubenVorsselmans@users.noreply.github.com>
2021-12-23 14:36:21 +0100

    shooter

---
+++ SaveTheCity/Assets/Scripts/Hit.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip audioClip;
+    public GameObject explosionParticle;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag =="Projectile")
+        {
+            Instantiate(explosionParticle, transform.position, transform.rotation);
+            if (audioClip)
+            {
+                if (gameObject.GetComponent<AudioSource>())
+                {
+                    gameObject.GetComponent<AudioSource>().PlayOneShot(audioClip);
+                }
+                else
+                {
+                    AudioSource.PlayClipAtPoint(audioClip, transform.position);
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
