735da912c1d9ee3aebfa1481c59d66101dc68f02
Tibe Desktop <tibe.de.clercq@gmail.com>
2022-01-07 14:54:21 +0100

    auto despawn bug fix

--- SaveTheCity/Assets/Scripts/ButtonLevel.cs
+++ SaveTheCity/Assets/Scripts/ButtonLevel.cs
@@ -9,6 +9,7 @@ public class ButtonLevel : MonoBehaviour
     public string _LevelToLoad;
     public void LoadLevel()
     {
+        MyGameManager.gameState = MyGameManager.GameStates.Playing;
         NewWaypointProgressTracker.isDriving = true;
         Timer.IsActive = true;
         MyGameManager.points = 0;
