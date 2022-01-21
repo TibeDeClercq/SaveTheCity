a32ffb07dc6e7d009d1850d42503e31cf3bb05ab
Tibe Desktop <tibe.de.clercq@gmail.com>
2022-01-21 17:02:00 +0100

    credits button added

---
+++ SaveTheCity/Assets/Scripts/CreditsButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsButtonScript : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://www.youtube.com/channel/UCR1zkRAN_mIlAZ8QpI_G_ng");
+    }
+}
